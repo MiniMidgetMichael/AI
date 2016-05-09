831b0f2e1fdeb27db91e5531737019d925285463
MiniMidgetMichael <child2dad@gmail.com>
2016-05-09 10:13:35 -0700

    added 'again' generator for functions

--- AI.py
+++ AI.py
@@ -152,6 +152,15 @@ class AI_(turtle.Turtle):
             with open("prefs.txt", "wb") as p:
                 pickle.dump(self.prefs, p)
 
+    def _run_again(self, act):
+        chance = self.chance
+        prefs = self.prefs
+        ## >>> [1, 0, 0, 1, etc.]
+        again = random.choice(range(act, (100-chance)))
+        if again == act:
+            ##prefers that option
+            return True
+
     def smart_act(self, t):
         prefs = self.prefs
         self._smart_gen_values()
@@ -162,7 +171,8 @@ class AI_(turtle.Turtle):
         prev_y = self.Turtle.ycor()
         func_params = self.func_params
         chance = self.chance
-        r_time = 0
+        times = 0
+        again = False
         if not(self._file_empty("params.txt")):
             with open("params.txt", "rb") as f:
                 f_func_params = pickle.load(f)
@@ -172,13 +182,29 @@ class AI_(turtle.Turtle):
                 f_prefs = pickle.load(f)
                 prefs.update(f_prefs)
 
-        while (r_time < t):
+        while (times < t):
+            again = False
             prev_x = self.Turtle.xcor()
             prev_y = self.Turtle.ycor()
-            for i in prefs.values():
-                if i >= 1:
-                    ##prefer that option
-                    again = random.choice(range(i))
+            for k,v in prefs.items():
+                if v >= 1:
+                    if self._run_again(v) == True:
+                        print ("#DO AGAIN: ")
+                        action = k
+                        if not(self._param_needed(getattr(self.Turtle, action)) is False):
+                            needed_param = self._param_needed(getattr(self.Turtle, action))
+                            fun = getattr(self.Turtle, action)
+                            working_param = self._working_param(fun, needed_param)
+                            print (action, working_param)
+                            if type(self._working_param(fun, needed_param)) is int:
+                                if ((self.Turtle.xcor() != prev_x) or (self.Turtle.ycor() != prev_y)):
+                                    ##print ("#MOVED")
+                                    prefs[action] += 1
+                        times += 1
+                        again = True
+            if again == True:
+                continue
+                        
             
             action = random.choice(range(chance, 100))
             ##print (action)
@@ -195,10 +221,12 @@ class AI_(turtle.Turtle):
                             ##print ("#MOVED")
                             prefs[action] += 1
 
-            time.sleep(0.5)
-            r_time += 0.5
 
-            
+            if not(again):
+                time.sleep(0.5)
+                times += 1
+            else:
+                time.sleep(0.5)
 
 
 screen = turtle.Screen()
