6165e9bee123169cc1f12d9339a424b89cdc5cb0
MiniMidgetMichael <child2dad@gmail.com>
2016-05-09 10:25:20 -0700

    added module for AI_target

---
+++ AI_target.py
@@ -0,0 +1,6 @@
+#! C:/Users/MichaelLFarwell/AppData/Local/Programs/Python/Python35-32/python.exe
+import random, math, turtle, time, inspect, pickle, os
+
+class AI_goal_(object):
+    def __init__(self):
+        pass
