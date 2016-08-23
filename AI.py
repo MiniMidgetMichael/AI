ee45413007a2c87b724a68e307f08c52e981cf99
Michael Farwell <child2dad@gmail.com>
2016-08-23 10:47:50 -0700

    Revert "Revert "fixed cycle list""
    This reverts commit caa7bd93a39a17ff9e8f0bc28475986642b4f1ef.

--- AI.py
+++ AI.py
@@ -363,6 +363,7 @@ class AI_(turtle.Turtle):
                             fun = getattr(self.Turtle, action)
                             working_param = self._new_working_param(fun, needed_param)
                             print (action, working_param)
+                            cycle.append([action, working_param])
                             curr_x = self.Turtle.xcor()
                             curr_y = self.Turtle.ycor()
                             curr_pos = [curr_x, curr_y]
@@ -378,7 +379,7 @@ class AI_(turtle.Turtle):
                         times += 1
                         again = True
             if again == True:
-                cycle.append([action, working_param])
+                
                 ##print ("#CYCLE IN 'smart_act': ", cycle)
                 if not(working_param is None):
                     func_params[action] = working_param
@@ -404,6 +405,7 @@ class AI_(turtle.Turtle):
                     fun = getattr(self.Turtle, action)
                     working_param = self._new_working_param(fun, needed_param)
                     print (action, working_param)
+                    cycle.append([action, working_param])
                     curr_x = self.Turtle.xcor()
                     curr_y = self.Turtle.ycor()
                     curr_pos = [curr_x, curr_y]
@@ -417,8 +419,6 @@ class AI_(turtle.Turtle):
                             prefs.setdefault(action, 0)
                         else:
                             prefs[action] += 1
-                    if not(action in cycle):
-                        cycle.append([action, working_param])
                     ##print ("#CYCLE IN 'smart_act': ", cycle)
                     if not(working_param is None):
                         func_params[action] = working_param
